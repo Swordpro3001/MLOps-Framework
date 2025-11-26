b17c41336c912ba5c629785fb2c4a591b75d0931
Franz Puerto <puertofranz@gmail.com>
2025-11-26 21:55:37 +0100

    [Feat]: Removed Teamcity leftovers and adjusted gitlab

--- init-db/init.sql
+++ init-db/init.sql
@@ -2,19 +2,25 @@
 
 -- Create GitLab database and user
 CREATE DATABASE gitlabhq_production;
-CREATE USER gitlab WITH ENCRYPTED PASSWORD '${POSTGRES_PASSWORD_GITLAB}';
+CREATE USER gitlab WITH ENCRYPTED PASSWORD 'gitlabpass';
 GRANT ALL PRIVILEGES ON DATABASE gitlabhq_production TO gitlab;
 
 -- Create MLflow database and user
 CREATE DATABASE mlflow;
-CREATE USER mlflow WITH ENCRYPTED PASSWORD '${POSTGRES_PASSWORD_MLFLOW}';
+CREATE USER mlflow WITH ENCRYPTED PASSWORD 'mlflowpass';
 GRANT ALL PRIVILEGES ON DATABASE mlflow TO mlflow;
 
 -- Switch to GitLab database for extensions
 \c gitlabhq_production;
 CREATE EXTENSION IF NOT EXISTS "pg_trgm";
 CREATE EXTENSION IF NOT EXISTS "btree_gist";
+ALTER DATABASE gitlabhq_production OWNER TO gitlab;
+
+-- Grant schema permissions
+GRANT ALL ON SCHEMA public TO gitlab;
 
 -- Switch to MLflow database for tables
 \c mlflow;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+ALTER DATABASE mlflow OWNER TO mlflow;
+GRANT ALL ON SCHEMA public TO mlflow;
